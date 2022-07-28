5b03184d819ab09421a70c4b76d5dd07dbc5ce35
Claudia Berrocal <claudiabg-c>
2022-07-28 22:05:25 +0200

    Add loader page html structure with styles. Add params for data in different languages.

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-loader',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderComponent implements OnInit {
 
-  constructor() { }
+  private debug: boolean = false;
+  public dataLang: any = [];
+  public lang: string = "";
 
-  ngOnInit(): void {
+  constructor(
+    private _activeRouter: ActivatedRoute,
+    private _dataAPI: DataService,
+  ) {
+    this._activeRouter.params.subscribe(parametros => {
+      if (this.debug) {console.log('*** LOADING PARAMS...', parametros);}
+      this.lang = parametros['lang'];
+    })
   }
 
+  ngOnInit(): void {
+    this.GetDataLang(this.lang);
+  }
+
+  GetDataLang(lang:string){
+    if (this.debug) {console.log('*** LOADING DATA LANG...');}
+    this._dataAPI.getContentLang().subscribe(res => {
+      this.dataLang = res[lang];
+
+      if (this.debug) {
+        console.log("****** DATA LANG: ", this.dataLang);
+      }
+    });
+  }
 }
