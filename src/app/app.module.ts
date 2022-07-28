3f8e19e40c647f91d3753c40a162fca822351ed4
Claudia Berrocal <claudiabg-c>
2022-07-28 19:26:55 +0200

    Delete angular default project page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Templates
+
 import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
