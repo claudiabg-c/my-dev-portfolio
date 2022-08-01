cdfeefa0eea46cb6436a8eb7ba0b4a6867b77dcd
Claudia Berrocal <claudiabg-c>
2022-08-01 19:56:14 +0200

    Add font awesome and fonts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { IndexComponent } from './components/index/index.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'es/index', pathMatch: 'full'},
   {path: ':lang/index', component: IndexComponent},
+  {path: ':lang/home', component: HomeComponent},
 
   {path: '**', redirectTo: 'es/index', pathMatch: 'full'}
 ];
