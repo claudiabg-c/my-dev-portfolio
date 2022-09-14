a6b67c3dd422853d0491f629897a39944e8d6f55
Claudia Berrocal <claudiabg-c>
2022-09-14 18:42:57 +0200

    Add html skeleton for the about component and start calling the data

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  private debug: boolean = false;
+
+  public lang: string = "";
+  public dataLang: any = [];
+
+  constructor(
+    private _activeRouter: ActivatedRoute,
+    private _dataAPI: DataService
+  ) {
+    this._activeRouter.params.subscribe(params => {
+      if (this.debug) {console.log('*** LOADING PARAMS...'); console.log(params);}
+      this.lang = params['lang'];
+    });
+  }
 
   ngOnInit(): void {
+    this.GetDataLang(this.lang);
+  }
+
+  GetDataLang(lang: string) {
+    this._dataAPI.getContentLang().subscribe(res => {
+      this.dataLang = res[lang];
+    });
   }
 
 }
