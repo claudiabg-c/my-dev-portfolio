a30eaf2348b5b3208a54019800b8a31a128046e4
Claudia Berrocal <claudiabg-c>
2022-11-06 21:45:19 +0100

    Add project initial data in content json and call it with the work component.

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-work',
@@ -11,26 +11,47 @@ export class WorkComponent implements OnInit {
 
   private debug: boolean = true;
 
+  public lang: string = '';
   public projectId: string = '';
   public projectsIndex: any = [];
+  public projects: any = [];
 
   constructor(
-    private _dataAPI: DataService,
-  ) { }
-
-  ngOnInit(): void {
-    this.GetIndex(this.projectId);
+    private _activeRouter: ActivatedRoute,
+    private _dataAPI: DataService
+  ) {
+    this._activeRouter.params.subscribe(params => {
+      this.lang = params['lang'];
+    });
   }
 
-  GetIndex(projectId: string) {
+  ngOnInit(): void {
+    this.GetIndex();
+    this.GetData();
+  }
+
+  GetIndex() {
     this._dataAPI.getIndex().subscribe(res => {
       this.projectsIndex = res.projects;
       let numberOfProjects = res.projects.length;
       if (this.debug) {
         console.log("*** TOTAL PROJECTS: ", numberOfProjects);
-        console.log("*** PROJECTS DATA:", this.projectsIndex);
+        console.log("*** PROJECTS INDEX:", this.projectsIndex);
       }
     });
   }
+  
+  GetData(){
+    if (this.debug) {console.log('*** LOADING DATA...');}
+    this._dataAPI.getContent().subscribe(res => {
+      this.projects = res[this.lang].projects;
+      if (this.debug) {
+        console.log("****** DATA: ", this.projects);
+      }
+      for (let i = 0; i < this.projectsIndex.length; i++) {
+        this.projectId = '"' + i+1 + '"';
+      }
 
+    });
+  }
 }
