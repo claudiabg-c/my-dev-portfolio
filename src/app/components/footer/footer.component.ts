52044f5c25e7f95b7c2f7c4e1780fa2ff1e628de
Claudia Berrocal <claudia.berrocalgomez@gmail.com>
2023-01-08 15:49:33 +0100

    Add styles and functionality footer component.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
+
+  private debug: boolean = true;
+
+  public year: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getYear();
   }
-
+  
+  getYear() {
+    this.year = new Date().getFullYear();
+  }
 }
